1aebfb7ee971104f760076cad46fcfb1363141f8
O.Cheremikin <O.Cheremikin@CZC724BBKX>
2021-11-03 11:02:42 +0300

    Добавьте файлы проекта.

---
+++ Task12/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                Console.Write("Введите радиус: ");
+                double Radius = Convert.ToDouble(Console.ReadLine());
+                if (Radius <= 0)
+                {
+                    Console.Write("Радиус должен быть положительным числом!");
+                    Console.ReadKey();
+                    return;
+                }
+                Console.Write("Введите координату х: ");
+                double X = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите координату у: ");
+                double Y = Convert.ToDouble(Console.ReadLine());
+                Circle.Length(Radius);
+                Circle.Square(Radius);
+                Circle.Position(Radius, X, Y);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Неверный формат!");
+                Console.ReadKey();
+                return;
+            }
+        }
+        static class Circle
+        {
+            public static void Length(double Radius)
+            {
+                double Length = Math.PI * 2 * Radius;
+                Console.WriteLine("Длина окружности: {0:f2}", Length);
+            }
+            public static void Square(double Radius)
+            {
+                double Square = Math.PI * Radius* Radius;
+                Console.WriteLine("Площадь круга: {0:f2}", Square);
+            }
+            public static void Position(double Radius, double X, double Y)
+            {
+                if (X <= Radius && Y <= Radius)
+                {
+                    Console.WriteLine("Точка ({0},{1}) принадлежит кругу с радиусом {2}", X, Y, Radius);
+                }
+                else
+                {
+                    Console.WriteLine("Точка ({0},{1}) не принадлежит кругу с радиусом {2}", X, Y, Radius);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
